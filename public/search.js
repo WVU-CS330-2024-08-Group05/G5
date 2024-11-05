924e4da8eed665a990578d190d52a8ddd7bd4510
Stephen Heflin <sh00135@mix.wvu.edu>
2024-11-04 19:09:19 -0500

    Search now handles resort names and partial names/states

--- public/search.js
+++ public/search.js
@@ -6,13 +6,21 @@ $(function () {
     console.log('ready...');
 
     $('#search-button').on('click', function () {
-        let state = $('#search-bar').val();
+        // Get search bar value
+        let search = $('#search-bar').val();
+        // Create url for /search.html
         let url = new URL('./search.html', baseUrl)
-        if (state) url.searchParams.append('state', state);
-
+        if (search) url.searchParams.append('search', search);
+        // Hide main, show search-results
+        $('#main').hide();
+        $('#search-results').show();
+        // Fetch search results
         console.log(`Fetching ${url.toString()}...`);
         fetch(url).then((res) => res.text())
-            .then((html) => $('#search-results').html(html));            ;
-    })
+            .then((html) => {
+                if (html) $('#search-results').html(html);
+                else $('#search-results').html('<h2>No results...</h2>');
+            });
+    });
 
 });
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const RESORTS = require('./resortdata.json');
-const STATES = getStates(RESORTS);
 
 // serve files from root dir
 app.use(express.static('public'));
@@ -15,27 +14,25 @@ app.listen(8080, function () {
     console.log('Listening on port 8080...');
 });
 
-// Search functionality
+/**
+ * Search functionality
+ * 
+ * Search results include resorts with names that start with req.query.search and resorts that are in a state the the begins with req.query.search.
+ */
 app.get('/search.html', function (req, res) {
-        if (req.query && req.query.state) {
-            res.send(generateSearchHtml(filterByState(RESORTS, req.query.state)));
+        if (req.query.search) {
+            res.send(generateSearchHtml(filterBySearch(RESORTS, req.query.search)));
         } else {
             res.send(generateSearchHtml(RESORTS))
         }
 });
 
-function getStates(resorts) {
-    let states = new Array();
-    for (let resort of resorts) {
-        states.push(resort.state);
-    }
-    return states;
-}
-
-function filterByState(resorts, state) {
+function filterBySearch(resorts, search) {
     let new_resorts = new Array();
     for (let resort of resorts) {
-        if (resort.state.toLowerCase() == state.toLowerCase())
+        if (resort.state.toLowerCase().startsWith(search.toLowerCase()))
+            new_resorts.push(resort);
+        if (resort.resort_name.toLowerCase().startsWith(search.toLowerCase()))
             new_resorts.push(resort);
     }
     return new_resorts;
@@ -65,4 +62,58 @@ function generateSearchHtml(resorts) {
 
 function filterByDistance(trips, location) {
     return;
+}
+
+state_abbreviation_to_name = {
+    "AK": "Alaska",
+    "AL": "Alabama",
+    "AR": "Arkansas",
+    "AZ": "Arizona",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "IA": "Iowa",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "MA": "Massachusetts",
+    "MD": "Maryland",
+    "ME": "Maine",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MO": "Missouri",
+    "MS": "Mississippi",
+    "MT": "Montana",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "NE": "Nebraska",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NV": "Nevada",
+    "NY": "New York",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VA": "Virginia",
+    "VT": "Vermont",
+    "WA": "Washington",
+    "WI": "Wisconsin",
+    "WV": "West Virginia",
+    "WY": "Wyoming",
+    "DC": "District of Columbia"
 }
