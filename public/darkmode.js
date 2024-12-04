996fe884359b6a410ad55e75c69249f1e7f484d6
LukeLehman <lukemlehman@icloud.com>
2024-12-04 10:09:33 -0500

    Some style changes

--- public/darkmode.js
+++ public/darkmode.js
@@ -1,21 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('darkModeToggle');
-
+    const toggleButton = document.getElementById('dark-mode-toggle');
     // Check localStorage and apply saved mode
     if (localStorage.getItem('darkMode') === 'enabled') {
         document.body.classList.add('dark-mode');
-        toggleButton.checked = true;
     }
     else {
         document.body.classList.remove('dark-mode');
-        toggleButton.checked = false;
     }
 
     // Toggle dark mode on slider change
-    toggleButton.addEventListener('change', () => {
+    toggleButton.on('click', () => {
 
         // Apply dark mode based on the slider state
         if (toggleButton.checked) {
+            console.log("dark mode clicked");
             document.body.classList.add('dark-mode');
             localStorage.setItem('darkMode', 'enabled');
         } else {
