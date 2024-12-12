0d9bcabdc6096d13001df0c5661b66d6190143c3
Stephen Heflin <sh00135@mix.wvu.edu>
2024-12-12 14:33:02 -0500

    restructured sql

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-const sql = require('mssql');
+const mssql = require('mssql');
 const bcrypt = require('bcrypt');
 const RESORTS = require('./resortdata.json');
 const NodeGeolocation = require('nodejs-geolocation').default;
 const geo = new NodeGeolocation('App');
 const Weather = require('./weather.js');
 const ResortCard = require('./ResortCard.js');
+const sql = require('./sql');
 
 MAX_RESULTS = 10;
 
@@ -28,47 +29,6 @@ app.listen(PORT, () => {
 
 // Middleware to parse json
 app.use(express.json());
-
-
-/**
- * Connect to Database
- */
-const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    database: process.env.DB_DATABASE,
-    options: {
-        encrypt: true, // Enables encryption for Azure SQL
-        enableArithAbort: true,
-    },
-};
-
-
-// establishing a persistent pool connection
-let poolConnection;
-
-async function initializePool() {
-    let retries = 5; // Set a retry limit
-    while (retries > 0) {
-        try {
-            poolConnection = await sql.connect(config);
-            console.log('Database connected');
-            break;
-        } catch (err) {
-            console.error('Database connection error:', err.message);
-            retries--;
-            if (retries === 0) {
-                throw new Error("Failed to connect to the database after multiple attempts.");
-            }
-            console.log(`Retrying connection in 5 seconds... (${retries} retries left)`);
-            await new Promise(resolve => setTimeout(resolve, 5000));
-        }
-    }
-}
-
-// Initial connection attempt
-initializePool();
 
 
 /**
@@ -141,7 +101,6 @@ function filterByDistance(trips, location, range) {
 
 
 /** Get resort names */
-
 app.get('/resort-names', async function (req, res) {
     let resorts = [...RESORTS];
 
@@ -154,11 +113,10 @@ app.get('/resort-names', async function (req, res) {
 
 
 
-/**  Login Functionality 
+/**  Login Functionality
  * 
  * check if username is in database, then check if password matches
 */
-
 app.post('/login', async function (req, res) {
     let msg = "";
     
@@ -178,42 +136,6 @@ app.post('/login', async function (req, res) {
     res.send(msg);
 });
 
-
-async function connectAndQueryUsername(username) {
-    try {
-        
-        const query = `SELECT id FROM Accounts WHERE username COLLATE Latin1_General_BIN = @username`;
-        const inputs = [{ name: 'username', type: sql.VarChar, value: username }];
-        const resultSet = await executeQuery(query, inputs);
-
-        return resultSet.recordset.length > 0 ? "User found" : "Username not found";
-    } catch (err) {
-        console.error('Username query error:', err.message);
-        return err.message;
-    }
-}
-
-async function connectAndQueryPassword(username, password) {
-    try {
-        const query = `SELECT password AS hash FROM Accounts WHERE username COLLATE Latin1_General_BIN = @username`;
-        const inputs = [{ name: 'username', type: sql.VarChar, value: username }];
-        const resultSet = await executeQuery(query, inputs);
-
-        // Check if a matching record was found
-        if (resultSet.recordset.length > 0) {
-            const hash = resultSet.recordset[0].hash;
-
-            // Compare provided password with hashed password
-            const isMatch = await isPasswordCorrect(password, hash);
-            return isMatch ? "Password is correct" : "Password is incorrect";
-        } else {
-            return "Password is incorrect"; // User not found or wrong password
-        }
-    } catch (err) {
-        console.error(err.message);
-        return "Error checking password";
-    }
-}
 
 /**
  * Sign Up functionality
@@ -243,28 +165,6 @@ app.post("/signing-up", async function (req, res) {
     res.send(msg);
 });
 
-
-async function connectAndInsertAccount(username, password, email) {
-    try {
-        const hashedPassword = await hashPassword(password); // Hash the password
-
-        const query = `
-            INSERT INTO Accounts (username, password, email, settings, pinned_resorts, trips)
-            VALUES (@username, @hashedPassword, @email, NULL, NULL, NULL) `; // empty jsons
-        const inputs = [
-            { name: 'username', type: sql.VarChar, value: username },
-            { name: 'hashedPassword', type: sql.VarChar, value: hashedPassword },
-            { name: 'email', type: sql.VarChar, value: email }
-        ];
-        const resultSet = await executeQuery(query, inputs);
-
-        // Check if the insert was successful
-        return resultSet.rowsAffected[0] > 0 ? "Account created successfully" : "Account creation failed";
-    } catch (err) {
-        console.error(err.message);
-        return "Error creating account";
-    }
-}
 
 /** Pulling Ratings */
 
@@ -313,33 +213,7 @@ app.post("/account-trips", async function (req, res) {
 });
 
 
-async function connectAndQueryTrips(username) {
-    let msg = null;
 
-    const query = `SELECT trips FROM Accounts WHERE username COLLATE Latin1_General_BIN = @username`;
-    const input = [{ name: 'username', type: sql.VarChar, value: username }];
-    const resultSet = await executeQuery(query, input);
-
-    if (resultSet.recordset && resultSet.recordset.length > 0) {
-        const tripsString = resultSet.recordset[0].trips;
-
-        // Ensure `tripsString` is valid JSON
-        if (tripsString) {
-            try {
-                const trips = JSON.parse(tripsString); // Parse JSON
-                msg = trips; // Assign parsed JSON to msg
-            } catch (error) {
-                console.error("Failed to parse JSON from database:", error);
-                msg = []; // Return empty array on parsing failure
-            }
-        } else {
-            msg = []; // Handle null or empty trips column
-        }
-    } else {
-        msg = []; // No records found
-    }
-    return msg;
-}
 
 
 app.post("/store-trips", async function (req, res) {
@@ -361,24 +235,6 @@ app.post("/store-trips", async function (req, res) {
 
     res.send(msg);
 });
-
-
-async function connectAndUpdateTrips(username, trips) {
-    const query = `UPDATE Accounts SET trips = @trips WHERE username = @username`;
-    const input = [
-        { name: 'username', type: sql.VarChar, value: username },
-        { name: 'trips', type: sql.NVarChar, value: typeof trips === 'string' ? trips : JSON.stringify(trips) }
-        // Ensure trips is stringified only if it's not already a string
-    ];
-
-    try {
-        await executeQuery(query, input);
-        return 'Trips successfully stored';
-    } catch (err) {
-        console.error('Error in connectAndUpdateTrips:', err);
-        return 'Failed to update trips';
-    }
-}
 
 /** Pin Resorts */
 
@@ -435,36 +291,3 @@ async function getPinnedResorts(username) {
 
     return msg; // Return the result
 }
-
-
-
-
-/** Query helper functions */
-
-async function executeQuery(query, inputs = []) {
-    if (!poolConnection) {
-        throw new Error("Database connection is not initialized.");
-    }
-    try {
-        const request = poolConnection.request();
-        inputs.forEach(({ name, type, value }) => {
-            request.input(name, type, value);
-        });
-
-        return await request.query(query);
-    } catch (err) {
-        console.error('Database query error:', err.message);
-        throw err; // Throw to handle error higher up
-    }
-}
-
-
-async function hashPassword(password) {
-    const saltRounds = 10;
-    return await bcrypt.hash(password, saltRounds);
-}
-
-async function isPasswordCorrect(password, hash) {
-    return await bcrypt.compare(password, hash);
-}
-
