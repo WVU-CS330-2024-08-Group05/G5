e1ef30c81ce18d4eab280370c1926d8ae0e35856
LukeLehman <lukemlehman@icloud.com>
2024-12-16 15:24:57 -0500

    Fixed small issue

--- public/DropdownMenu.js
+++ public/DropdownMenu.js
@@ -1,5 +1,7 @@
+
 async function getNames() {
-    const url = urlBase + '/resort-names';
+    const baseUrl = 'http://localhost:8080';
+    const url = baseUrl + '/resort-names';
     console.log(`Getting from ${url}...`);
     const response = await fetch('http://localhost:8080/resort-names');
 
